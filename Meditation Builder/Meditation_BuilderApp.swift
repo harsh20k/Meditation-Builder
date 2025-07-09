97cc87b98995fdc3a02cc88b732df95da7369a9e
harsh20k <harsh.pandey77@gmail.com>
2025-07-09 14:00:00 +0530

    Initial Commit

---
+++ Meditation Builder/Meditation_BuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meditation_BuilderApp.swift
+//  Meditation Builder
+//
+//  Created by harsh  on 09/07/25.
+//
+
+import SwiftUI
+
+@main
+struct Meditation_BuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
