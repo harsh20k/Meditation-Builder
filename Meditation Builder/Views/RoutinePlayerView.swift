0979c980efc074b0af216eac22686f778271aa63
harsh20k <harsh.pandey77@gmail.com>
2025-07-28 14:24:00 +0530

    Routine selection with carousal working,
    The timer is not handling selection from library view.

--- Meditation Builder/Views/RoutinePlayerView.swift
+++ Meditation Builder/Views/RoutinePlayerView.swift
@@ -186,7 +186,7 @@ struct PreSessionState: View {
             ZStack {
                 // Player Header
                 PlayerHeaderView(
-                    title: viewModel.savedRoutines.count > 1 ? "Select Routine" : viewModel.routineData.name,
+                    title: viewModel.routineData.name,
                     onClose: onClose
                 )
                 
